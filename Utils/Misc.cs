0703652ecd7a7606b1a4586a1e85347f65818855
FrozenFog <97frozenfog@gmail.com>
2019-12-01 09:13:45 +0800

    Const Upgrade

--- Utils/Misc.cs
+++ Utils/Misc.cs
@@ -8,9 +8,6 @@ namespace relert_sharp.Utils
 {
     public class Misc
     {
-        private static readonly string[] booltrue = { "yes", "True", "true"};
-        private static readonly string[] boolfalse = { "no", "False", "false" };
-        private static readonly string[] nullstring = { "<none>", "None", "none" };
         public static bool IsList(string s)
         {
             return s.Contains(",");
@@ -23,6 +20,92 @@ namespace relert_sharp.Utils
         {
             string[] tmp = s.Split(new char[1] { c }, t+1);
             return tmp;
+        }
+    }
+    public static class Cons
+    {
+        public static class EntName
+        {
+            public static readonly string[] SystemEntity =
+            {
+                "General","JumpjetControls","SpecialWeapons","GenericPrerequisites","AudioVisual","CrateRules","Powerups","CombatDamage","Radiation","ElevationModel",
+                "WallModel","GlobalControls","MultiplayerDialogSettings","Maximums","AI","IQ","Easy","Normal","Difficult","MouseCursors","Colors","ColorAdd","ArmorTypes",
+                "Sides","GDI","Nod","ThirdSide","FourthSide","UnitedStates","Europeans","Pacific","USSR","Latin","Chinese","PsiCorps","ScorpionCell","Headquaters","Guild1",
+                "Guild2","Guild3","Special","Neutral","Defaults"
+            };
+            public static readonly string[] DictionaryList =
+            {
+                "InfantryTypes","VehicleTypes","BuildingTypes","TerrainTypes","SmudgeTypes","OverlayTypes","Animations","VoxelAnims","Particles","ParticleSystems",
+                "SuperWeaponTypes","Warheads","WeaponTypes","Projectiles","Countries","VariableNames","Movies","SoundList","Themes","PreviewPack","Houses","IsoMapPack5",
+                "OverlayDataPack","OverlayPack","Digest"
+            };
+        }
+        public static readonly string[] BoolTrue = { "yes", "True", "true" };
+        public static readonly string[] BoolFalse = { "no", "False", "false" };
+        public static readonly string[] NullString = { "<none>", "None", "none" };
+        public static class KeyName
+        {
+            public static readonly string[] IntKey =
+            {
+                "Bounty.Value","Cost","PhysicalSize","Points","Sight","Size","Soylent","Speed","Strength","ThreatPosed","GuardRange","DetectDisguiseRange",
+                "Adjacent","TechLevel","Ammo","Reload","UndeployDelay","AirRangeBonus","JumpjetSpeed","JumpjetClimb","JumpJetAccel","BuildLimit","SensorsSight",
+                "Passengers","Weight","InitialPayload.Nums","AttachEffect.Duration","MaxDebris","ROT","Power","MinDebris","FireAngle","TurretCount","WeaponCount",
+                "DebrisMaximums","Damage","ROF","LaserDuration","EMP.Duration","EMP.Cap","AmbientDamage","Burst","DisableWeapons.Duration","EmptyReload","MaxNumberOccupants",
+                "Money.Amount","SizeLimit","Sonar.Duration","TurretROT"
+            };
+            public static readonly string[] FloatKey =
+            {
+                "BuildTimeMultiplier","Range","MinimumRange","CellSpread","PercentAtMax","AttachEffect.SpeedMultiplier","AttachEffect.ArmorMultiplier",
+                "AttachEffect.FirepowerMultiplier","BallisticScatter.Max","BallisticScatter.Min","DeathWeaponDamageModifier","IronCurtain.Modifier"
+            };
+            public static readonly string[] PercentListKey =
+            {
+                "Verses"
+            };
+            public static readonly string[] PercentKey =
+            {
+                "ProneDamage"
+            };
+            public static readonly string[] SpaceListKey =
+            {
+                "Sounds","Control","FShift","Priority","Type","Delay"
+            };
+        }
+        public static class Interpreter
+        {
+            public static readonly string[] SightLike =
+            {
+                "Sight","Size","AirRangeBonus","ThreatPosed","Soylent","Bounty.Value","JumpjetSpeed","JumpjetClimb","AmbientDamage","Ammo","AttachEffect.Duration",
+                "BallisticScatter.Max","BallisticScatter.Min","BuildLimit","Burst","BuildTimeMultiplier","CellSpread","Cost","Damage","DeathWeaponDamageModifier",
+                "DebrisMaximums","DisableWeapons.Duration","EMP.Cap","EMP.Duration","EmptyReload","Fearless","GuardRange","IronCurtain.Modifier","LaserDuration","MaxDebris",
+                "MaxNumberOccupants","MinDebris","MinimumRange","Money.Amount","Passengers","PercentAtMax","Points","Power","Range","Reload","ROF","ROT","SizeLimit",
+                "Sonar.Duration","Speed","Strength","TurretROT","UndeployDelay","Weight"
+            };
+            public static readonly string[] ActiveBoolLike =
+            {
+                "AffectsAllies","AffectsOwner","Bounty.Display","Bright","Deployer","DeployFire","ImmuneToPsionics","ImmuneToRadiation","Malicious","OmniFire","OpportunityFire",
+                "RadarInvisible","Rocker","Selectable","Trainable","Wall","Wood"
+            };
+            public static readonly string[] PassiveBoolLike =
+            {
+                "Bounty","CanBeReversed"
+            };
+            public static readonly string[] AcquireBoolLike =
+            {
+                "Fearless"
+            };
+        }
+
+
+
+        public enum Language
+        {
+            EnglishUS, Chinese
+        }
+        public enum FileExtension
+        {
+            Undefined,CSV,TXT,YRM,MAP,INI,LANG,
+            UnknownBinary
         }
     }
 }
