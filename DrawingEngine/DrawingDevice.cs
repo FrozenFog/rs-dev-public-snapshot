b25cd356a43fc3c5ece86a53c87904f06e5e98cc
FrozenFog <97frozenfog@gmail.com>
2019-12-28 15:44:17 +0800

    Revert "Revert "-begin GUI editing""
    This reverts commit 29e9fb3b88caf51e96e0dd0573e2a198fb9fc8c2.

---
+++ DrawingEngine/DrawingDevice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace relert_sharp.DrawingEngine
+{
+    public class DrawingDevice
+    {
+
+    }
+}
