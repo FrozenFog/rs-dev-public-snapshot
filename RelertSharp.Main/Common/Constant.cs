343502030a5102b2eda1b3a3d7283f43644f5401
FrozenFog <97frozenfog@gmail.com>
2020-06-10 01:37:00 +0800

    fix release

--- RelertSharp.Main/Common/Constant.cs
+++ RelertSharp.Main/Common/Constant.cs
@@ -8,7 +8,7 @@ namespace RelertSharp.Common
 {
     public static class Constant
     {
-        public static string ReleaseDate = " - ver. 2020.06.09-2237 - UNSTABLE";
+        public static string ReleaseDate = " - ver. 2020.06.10-0124 - UNSTABLE";
         public static class MapStructure
         {
             public static int ArgLenInfantry = 14;
