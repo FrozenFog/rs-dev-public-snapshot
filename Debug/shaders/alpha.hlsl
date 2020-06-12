6eda1bc26d6629987f0ff4f58985e2c41c1121ec
FrozenFog <97frozenfog@gmail.com>
2020-06-12 22:19:25 +0800

    -

--- Debug/shaders/alpha.hlsl
+++ Debug/shaders/alpha.hlsl
@@ -1,19 +1,25 @@
 sampler2D default_sampler : register(s0);
-uniform vector alpha_cof;
+sampler2D alphasurf_sampler : register(s1);
 
-void amain(in float2 texcoord:TEXCOORD,out vector color:COLOR)
+struct PSOut
 {
-    float inindex = tex2D(default_sampler, texcoord).r * (255. / 256) + (0.5 / 256);
+    vector color : COLOR;
+    float depth : DEPTH;
+};
+
+PSOut amain(in float2 texcoord : TEXCOORD, in float2 screenpos : TEXCOORD1)
+{
+    PSOut output = (PSOut) 0;
     
-    if(inindex == 127.0 / 256)
+    float inindex = tex2D(default_sampler, texcoord).r * (255. / 256) + (0.5 / 256);
+    float currenta = tex2D(alphasurf_sampler, screenpos).r;
+    
+    if (inindex == 127 / 255.0)
         discard;
     
-    float cvalue = inindex <= 127.0 / 256 ? 0.0 : 1.0;
-    vector outcolor = { cvalue, cvalue, cvalue, abs(0.5 - inindex) / 0.5 };
-    outcolor.r = outcolor.r * alpha_cof.r;
-    outcolor.g = outcolor.g * alpha_cof.g;
-    outcolor.b = outcolor.b * alpha_cof.b;
-    outcolor.a = outcolor.a * alpha_cof.a;
+    output.color.r = saturate(currenta * inindex / 0.5);
+    output.color.a = 1.0;
+    output.depth = 0.0;
     
-    color = outcolor;
+    return output;
 }
--- RelertSharp.Main/Common/Constant.cs
+++ RelertSharp.Main/Common/Constant.cs
@@ -8,7 +8,7 @@ namespace RelertSharp.Common
 {
     public static class Constant
     {
-        public static string ReleaseDate = " - ver. 2020.06.10-2147 - UNSTABLE";
+        public static string ReleaseDate = " - ver. 2020.06.11-0050 - UNSTABLE";
         public static class MapStructure
         {
             public static int ArgLenInfantry = 14;
