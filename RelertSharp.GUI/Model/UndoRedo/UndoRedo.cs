1cc796d74470fc8a92d556657e7f83f65cd6e4df
FrozenFog <97frozenfog@gmail.com>
2020-08-13 00:21:08 +0800

    undoredo LinkedList struct

--- RelertSharp.GUI/Model/UndoRedo/UndoRedo.cs
+++ RelertSharp.GUI/Model/UndoRedo/UndoRedo.cs
@@ -16,34 +16,44 @@ namespace RelertSharp.GUI.Model
     }
     internal class UndoRedo
     {
-        private readonly Stack<UndoRedoCommand> _UndoStack = new Stack<UndoRedoCommand>();
-        private readonly Stack<UndoRedoCommand> _RedoStack = new Stack<UndoRedoCommand>();
+        private readonly LinkedList<UndoRedoCommand> _UndoList = new LinkedList<UndoRedoCommand>();
+        private readonly LinkedList<UndoRedoCommand> _RedoList = new LinkedList<UndoRedoCommand>();
 
         public UndoRedo() { }
 
         public void Undo()
         {
-
+            if (_UndoList.Count > 0)
+            {
+                UndoRedoCommand cmd = _UndoList.Last();_UndoList.RemoveLast();
+                _RedoList.AddLast(cmd);
+                ExecuteCommand(cmd.CommandType, cmd.PrevParam);
+            }
         }
 
         public void Redo()
         {
-
+            if (_RedoList.Count > 0)
+            {
+                UndoRedoCommand cmd = _RedoList.Last(); _RedoList.RemoveLast();
+                _UndoList.AddLast(cmd);
+                ExecuteCommand(cmd.CommandType, cmd.NowParam);
+            }
         }
 
         public void PushCommand(UndoRedoCommand cmd)
         {
-
+            if (_RedoList.Count > 0) _RedoList.Clear();
+            if (_UndoList.Count == 50) _UndoList.RemoveFirst();
+            _UndoList.AddLast(cmd);
         }
         public void PushCommand(UndoRedoCommandType type, IEnumerable<object> paramPrev, IEnumerable<object> paramNow)
         {
             PushCommand(new UndoRedoCommand(type, paramPrev, paramNow));
         }
-    }
 
-    internal class UndoRedoCommand
-    {
-        public UndoRedoCommand(UndoRedoCommandType type, IEnumerable<object> paramPrev, IEnumerable<object> paramNow)
+
+        private void ExecuteCommand(UndoRedoCommandType type, IEnumerable<object> param)
         {
 
         }
---
+++ RelertSharp.GUI/Model/UndoRedo/UndoRedoCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelertSharp.GUI.Model
+{
+    internal class UndoRedoCommand
+    {
+        public UndoRedoCommand(UndoRedoCommandType type, IEnumerable<object> paramPrev, IEnumerable<object> paramNow)
+        {
+            CommandType = type;
+            PrevParam = paramPrev;
+            NowParam = paramNow;
+        }
+
+
+        #region Public Calls - UndoRedoCommand
+        public UndoRedoCommandType CommandType { get; private set; }
+        public IEnumerable<object> PrevParam { get; private set; }
+        public IEnumerable<object> NowParam { get; private set; }
+        #endregion
+    }
+}
