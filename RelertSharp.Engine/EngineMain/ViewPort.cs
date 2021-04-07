48e961454db7a6ebf0e7d1febccf4070c8f13c6d
FrozenFog <97frozenfog@gmail.com>
2021-04-08 00:30:17 +0800

    fix a cell finding issue, cell finding will begin from level 24 to level 0, ignore max height limit

--- RelertSharp.Engine/EngineMain/ViewPort.cs
+++ RelertSharp.Engine/EngineMain/ViewPort.cs
@@ -92,7 +92,7 @@ namespace RelertSharp.Engine
             scenepos += _NormTileVec * Constant.DrawingEngine.MapMaxHeightDrawing;
             int height = 0;
             Vec3 tileCoord = new Vec3();
-            for (; height < Constant.DrawingEngine.MapMaxHeightDrawing * 2; height++)
+            for (; tileCoord.Z >= 0; )
             {
                 tileCoord = ScenePosToCoord(scenepos);
                 if (_cellFindingReferance.HasTileOn(tileCoord))
